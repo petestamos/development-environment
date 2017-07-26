7d887f0bf3c610967bbd577db4229d71a5dbaa58
Peter Stamos <pstamos@admins-MacBook-Pro.local>
2017-07-26 16:34:23 -0400

    Mocking Checkpoint

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -21,6 +21,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // Hardcoding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstName": "John", "lastName": "Doe", "email": "john@gmail.com"},
+    {"id": 3, "firstName": "Jack", "lastName": "Smith", "email": "jack@gmail.com"}
+  ]);
+});
+
 // tell express to listen to port
 app.listen(port, function(err) {
   if (err) {
